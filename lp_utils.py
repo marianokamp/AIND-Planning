ce3b5604292b28170ac1e90d9344a2ba1f98c38b
Mariano Kamp <mariano.kamp@gmail.com>
2017-05-13 21:51:27 +0200

    Adde cargo 3 problem.

--- lp_utils.py
+++ lp_utils.py
@@ -49,7 +49,7 @@ def encode_state(fs: FluentState, fluent_map: list) -> str:
             state_tf.append('T')
         else:
             state_tf.append('F')
-    print("encode_state() fs=", fs, "fmap",  fluent_map, "result", "".join(state_tf)) #DEBUG
+    #print("encode_state() fs=", fs, "fmap",  fluent_map, "result", "".join(state_tf)) #DEBUG
     return "".join(state_tf)
 
 
